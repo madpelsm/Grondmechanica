ca52b2e73d69fa2a554cc85189278b5ca85cccd8
Mathias De Pelsmaeker <mathiasdepelsmaeker@hotmail.com>
2016-11-21 01:35:13 +0100

    necessary changes

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,15 +1,4 @@
-/*
-src/example2.cpp -- C++ version of an example application that shows
-how to use the form helper class. For a Python implementation, see
-'../python/example2.py'.
 
-NanoGUI was developed by Wenzel Jakob <wenzel.jakob@epfl.ch>.
-The widget drawing code is based on the NanoVG demo application
-by Mikko Mononen.
-
-All rights reserved. Use of this source code is governed by a
-BSD-style license that can be found in the LICENSE.txt file.
-*/
 
 #include <nanogui/nanogui.h>
 #include <iostream>
@@ -366,7 +355,6 @@ std::vector<std::string> split(const std::string &s, char delim) {
     std::string item;
     std::vector<std::string> elems;
     while (std::getline(ss, item, delim)) {
-        //elems.push_back(item);
         elems.push_back(std::move(item));
     }
     return elems;
